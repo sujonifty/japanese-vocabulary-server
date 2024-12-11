3adb7295083635459d9e351acb996caae3312530
sujonifty <sujonifty@gmail.com>
2024-12-11 22:41:59 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER)
+
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
